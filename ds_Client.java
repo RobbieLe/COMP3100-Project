175c0ffdaf245e17c5454bbf1375563cfc21ef95
RobbieLe <robinson.le2001@gmail.com>
2021-04-16 20:37:26 +1000

    Reverting last commit, pushed to the wrong branch before

--- ds_Client.java
+++ ds_Client.java
@@ -17,17 +17,17 @@ public class ds_Client {
         String jobString = "";  //Keeping job assignment in this variable for later use in scheduling
         int serverCount = 0;
 
-        while (!state.equals("Quitting")) {       //Looping for the whole communication process until the client is going to quit
-            if (!state.equals("")) {      //Ensures that it doesn't read anything in for the initial HELO communication
+        while (state != "Quitting") {       //Looping for the whole communication process until the client is going to quit
+            if (state != "") {      //Ensures that it doesn't read anything in for the initial HELO communication
                 inString = socketIn.readLine();
             }
 
             switch (state) {
                 case "":        //First communication to the server
                     outString = "HELO";     //Send HELO to initiate communication procotol
-                    state = "Authorisation";      //Change it to Authorisation to represent that it finished changing the required items for this stage
+                    state = "Initial";      //Change it to Initial to represent that it finished changing the required items for this stage
                     break;
-                case "Authorisation":     //Authorisation with the server
+                case "Initial":     //Authorisation with the server
                     System.out.println("Type your name.");      //Send to user, to alert for input of name for AUTH message
                     state = "Authorised";
                     break;
@@ -36,17 +36,10 @@ public class ds_Client {
                     state = "Ready";
                     break;
                 case "Ready":       //Retrives a list of the servers
-                    if (readSystemList(inString, serverList, socketIn, serverCount) == false) {
-                        outString = "GETS All";
-                        state = "SysInfoPrep";
-                    } else {
-                        state = "JobSchedule";
-                    }
-                    if (inString.contains("JOBN")) {
-                        jobString = inString;       //Keep the job assigned to client for later use
-                    }
+                    outString = "GETS All";
+                    jobString = inString;       //Keep the job assigned to client for later use
+                    state = "SysInfoPrep";
                     break;
-
                 case "SysInfoPrep":     //Get list of servers via DATA header
                     serverCount = getServerCount(inString);     //Get the total amount of servers existing
                     outString = "OK";
@@ -55,49 +48,28 @@ public class ds_Client {
                 case "SysInfoReading":       //Gets the list of the servers one-by-one     
                     readSystemList(inString, serverList, socketIn, serverCount);    //Calling this method to add servers to list
                     outString = "OK";
-                    state = "JobSchedule";
+                    state = "DoneSysInfo";
                     break;
-                
-                
-
-                case "JobSchedule":     //Finished reading list, Need to implement job scheduling
+                case "DoneSysInfo":     //Finished reading list, Need to implement job scheduling
                     
-                    outString = "SCHD " + getJobID(jobString) + " " + getLargestServer(serverList);
-                    System.out.println("OutString: " + outString + "\n");
-                    state = "Authorised";
-                    break;
-                case "JobScheduled":
-                    if (inString.equals("OK")) {
-                        outString = "REDY";
-                    }
-                    state = "NewJob";
-                    break;
-                case "NewJob":
-                    if (inString.contains("JCPL")) {
-                        dout.write(("REDY" + "\n").getBytes());
-                        inString = socketIn.readLine();
-
-                    } else
-                        jobString = inString;       //Keep the job assigned to client for later use
-
-                    if (!inString.contains("NONE")) {
-                        state = "DoneSysInfo";
-                    } else
-                        state = "Quitting";
+                    //CHANGE THESE FIELDS BELOW, TO IMPLEMENT SCHEDULING
+                
+                    outString = "QUIT";     //Change to schedule when implementing it
+                    state = "Quitting";     //Change to something more relevant to scheduling
                     break;
                 default:
                     System.out.println("Error has occurred");       //Hopefully we don't get here, but here as a safeguard
                     quitCommunication(din, dout, s);        //Close connection, if something bad happens
             }
 
-            if (state.equals("Authorised")) {        //If we are in AUTH stage, ensure we are sending the right message with the User's name
+            if (state == "Authorised") {        //If we are in AUTH stage, ensure we are sending the right message with the User's name
             dout.write(("AUTH " + br.readLine() + "\n").getBytes());
             } else {
             dout.write((outString + "\n").getBytes());      //Send the message set by the switch(outString) with its various states
             }
         }
         
-        if (state.equals("Quitting")) {      //Finished with while loop, proceeding with closing connection with server
+        if (state == "Quitting") {      //Finished with while loop, proceeding with closing connection with server
             quitCommunication(din, dout, s);    //Close connection to finish the communication
         }
     }
@@ -110,17 +82,13 @@ public class ds_Client {
         return serverCount;     //The number of servers existing
     }
 
-    private static boolean readSystemList(String inString, ArrayList<String> serverList, BufferedReader socketIn, int serverCount) throws IOException {
+    private static void readSystemList(String inString, ArrayList<String> serverList, BufferedReader socketIn, int serverCount) throws IOException {
         for (int i = 0; i < serverCount; i++) {     //Looping with result from getServerCount() as limit
             if (i != 0) {       //Ensure we are not reading with the initial loop to prevent skipping records of the server list, since we have already read it with the outer while loop
                 inString = socketIn.readLine();     //Read the server record and store into this String
             }
             serverList.add(inString);       //Adding the server record into the arrayList for future processing (getting the largest server)
         }
-        if (serverList.isEmpty()) {
-            return false;
-        } else
-        return true;
     }
 
     //Closing the connections with the server
@@ -133,30 +101,7 @@ public class ds_Client {
     //To be completed, return the server with the largest core count 
     //  (joon           0       active      97          1           15300   60200   0               1) 
     //  (Server-type    ID      State       Start-time  Core-count  Memory  Disk    Waiting-jobs    Running-jobs)
-    private static String getLargestServer(ArrayList<String> serverList) {
-        String largestServer = serverList.get(0);
-        String[] unparsedString1, unparsedString2;
-        String[] server;
-
-        for (int i = 0; i < serverList.size() -1; i++) {
-            unparsedString1 = serverList.get(i).split(" ");
-            unparsedString2 = serverList.get(i+1).split(" ");
-
-            if (Integer.parseInt(unparsedString1[4]) < Integer.parseInt(unparsedString2[4])) {
-                largestServer = serverList.get(i+1);
-            }
-        }
-        server = largestServer.split(" ");
-        return server[0] + " " + server[1];        //We want to return the Server-type and ID
-    }
-
-    //To be completed, return the JobID given by JobN message from server
-    // (JOBN    172         4       320         2       50      120)
-    // (JOBN    submitTime  jobID   estRuntime  core    memory  disk)
-    private static String getJobID(String jobString) {
-        String jobID = "";
-        String[] unparsedString = jobString.split(" ");      //Getting the words in the inString whilst ignore spaces to add into the array
-        jobID = unparsedString[2];
-        return jobID;   //return the 3rd word of the JOBN message, should be the JOBID
+    private static String getLargestServer() {
+        return null;        //We want to return the Server-type and ID
     }
 }
